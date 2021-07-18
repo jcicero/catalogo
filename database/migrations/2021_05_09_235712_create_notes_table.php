4039b16382f3703376409b570419292f454d3350
Jose Cicero Rocha Cavalcante <jose.cicero@uncisal.edu.br>
2021-07-18 19:12:02 -0300

    Criar notificacao para usuario

--- database/migrations/2021_05_09_235712_create_notes_table.php
+++ database/migrations/2021_05_09_235712_create_notes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNotificationsTable extends Migration
+class CreateNotesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateNotificationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('notifications', function (Blueprint $table) {
+        Schema::create('notes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('anvisa')->nullable();
@@ -22,11 +22,13 @@ class CreateNotificationsTable extends Migration
             $table->string('setor');
             $table->string('profissional')->nullable();
             $table->string('registroanvisa')->nullable();
-            $table->foreignId('brand_id')->constrained();
-            $table->foreignId('product_id')->constrained();
+            $table->foreignId('brand_id')->nullable();
+            $table->foreignId('product_id')->nullable();
             $table->string('lote')->nullable();
             $table->string('validade')->nullable();
             $table->text('queixa');
+            $table->text('email');
+            $table->text('produtodesc');
             $table->foreignId('user_id')->constrained();
 
         });
